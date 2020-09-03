37bb891f1fb2658dd1b2f03c82ef318390b34234
Sher <sher.sheikh77@gmail.com>
2020-09-02 23:11:52 -0400

    Change navigation color based on scroll area

--- main.js
+++ main.js
@@ -1,11 +1,54 @@
 const anchors = document.querySelectorAll('a[href^="#"]');
-const nav = document.getElementById('projectsNav');
+const nav = document.getElementById("projectsNav");
 
-anchors.forEach(anchor => {
-    anchor.addEventListener('click', e => {
-        anchor.scrollIntoView({
-            behavior: "smooth",
-            inline: 'center'
-        });
-    })
-})
+const onScroll = function () {
+  const scrollHeight = document.getElementById("project-reddit").scrollHeight;
+  if (window.scrollY >= 0 && window.scrollY < scrollHeight) {
+    document.querySelector(".selected").classList.remove("selected");
+    document.getElementById("redditClone").classList.add("selected");
+    document.body.style.setProperty(
+        "--currentColor",
+        getComputedStyle(document.body).getPropertyValue("--breddit")
+      );
+  } else if (
+    window.scrollY >= scrollHeight &&
+    window.scrollY < scrollHeight * 2
+  ) {
+    document.querySelector(".selected").classList.remove("selected");
+    document.getElementById("weatherCheck").classList.add("selected");
+    document.body.style.setProperty(
+        "--currentColor",
+        getComputedStyle(document.body).getPropertyValue("--weather")
+      );
+  } else if (
+    window.scrollY >= scrollHeight * 2 &&
+    window.scrollY < scrollHeight * 3
+  ) {
+    document.querySelector(".selected").classList.remove("selected");
+    document.getElementById("todoList").classList.add("selected");
+    document.body.style.setProperty(
+      "--currentColor",
+      getComputedStyle(document.body).getPropertyValue("--todo")
+    );
+  } else if (window.scrollY >= scrollHeight * 3) {
+    document.querySelector(".selected").classList.remove("selected");
+    document.getElementById("teaTime").classList.add("selected");
+    document.body.style.setProperty(
+        "--currentColor",
+        getComputedStyle(document.body).getPropertyValue("--tea")
+      );
+  }
+};
+
+window.addEventListener("scroll", onScroll);
+
+anchors.forEach((anchor) => {
+  anchor.addEventListener("click", (e) => {
+    document.querySelector(".selected").classList.remove("selected");
+    anchor.classList.add("selected");
+    anchor.scrollIntoView({
+      behavior: "smooth",
+      inline: "center",
+    });
+  });
+});
